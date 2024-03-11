3b01a600bf873fbbafb4d1b41882c2e6205b443a
RaviShekharThakur <ravissj10@gmail.com>
2024-03-11 18:19:23 +0530

    integrated flowbite and react

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { Alert } from 'flowbite-react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,9 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">pages/index.js</code>
         </p>
+        <Alert color="success" onDismiss={() => alert('Alert dismissed!')}>
+          <span className="font-medium">Info alert!</span> Change a few things up and try submitting again.
+        </Alert>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
