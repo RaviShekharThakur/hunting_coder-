bcfe69abe6221d59f77e11e73229d08bc230dc62
RaviShekharThakur <ravissj10@gmail.com>
2024-03-11 20:07:35 +0530

    flowbite navbar

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,42 @@
+'use client';
 import Image from "next/image";
+import Head from "next/head";
+import Link from "next/link";
 import { Inter } from "next/font/google";
-import { Alert } from 'flowbite-react';
+import { Button, Navbar } from 'flowbite-react';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
+    <div>
+    <Head>
+      <title>Hunting Coder</title>
+    </Head>
+    <Navbar fluid rounded>
+      <Navbar.Brand href="https://flowbite-react.com">
+        {/* <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" /> */}
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        <Button>Get started</Button>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Link href="/" passHref>
+            <Navbar.Link >Home</Navbar.Link>
+          </Link>
+          <Link href="/about" passHref>
+            <Navbar.Link>About</Navbar.Link>
+          </Link>
+          <Link href="/blog" passHref>
+            <Navbar.Link>Blog</Navbar.Link>
+          </Link>
+          <Link href="/contact" passHref>
+            <Navbar.Link>Contact</Navbar.Link>
+        </Link>
+      </Navbar.Collapse>
+    </Navbar>
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
@@ -14,9 +45,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">pages/index.js</code>
         </p>
-        <Alert color="success" onDismiss={() => alert('Alert dismissed!')}>
-          <span className="font-medium">Info alert!</span> Change a few things up and try submitting again.
-        </Alert>
+
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -47,76 +76,22 @@ export default function Home() {
           priority
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="blogs">
+        <div className="blogItem">
+          <h1>How do you do</h1>
+        </div>
+        <div className="blogItem">
+          <h1>How do you do</h1>
+        </div>
+        <div className="blogItem">
+          <h1>How do you do</h1>
+        </div>
+        <div className="blogItem">
+          <h1>How do you do</h1>
+        </div>
       </div>
+
     </main>
+    </div>
   );
 }
